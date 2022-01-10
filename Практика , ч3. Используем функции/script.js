cc0b5eeed00e7a10b34aa3840a16c2a20a2c81f6
Timur <timur.yakhin.99@gmail.com>
2022-01-10 21:24:13 +0300

    addfunction showMyDB

--- Практика , ч3. Используем функции/script.js
+++ Практика , ч3. Используем функции/script.js
@@ -12,5 +12,55 @@ genres
 P.S. Функции вызывать не обязательно*/
 
 'use strict';
-
+let numberOfFilms;
 // Код возьмите из предыдущего домашнего задания
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let nameFilm = prompt('Один из последних просмотренных фильмов?', ''),
+            score;
+        if (nameFilm != '' && nameFilm != null && nameFilm.length < 50) {
+            score = prompt('На сколько оцените его?', '');
+            personalMovieDB.movies[nameFilm] = score;
+        }else{
+            i--;
+        }    
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count<10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count>=10  && personalMovieDB.count<30) {
+        alert("Вы классический зритель");
+    } else if(personalMovieDB.count>30) {
+        alert("Вы киноман");
+    } else{
+        alert("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+function showMyDB(){
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
